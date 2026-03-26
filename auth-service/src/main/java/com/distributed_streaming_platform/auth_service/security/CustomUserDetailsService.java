8e586b5ed0c59a179ce5f5e298032d6a762bb8a8
98001yash <yashchauhan.gaya@gmail.com>
2026-03-26 15:53:32 +0530

    added the spring security in the auth-service

--- auth-service/src/main/java/com/distributed_streaming_platform/auth_service/security/CustomUserDetailsService.java
+++ auth-service/src/main/java/com/distributed_streaming_platform/auth_service/security/CustomUserDetailsService.java
@@ -4,26 +4,25 @@ package com.distributed_streaming_platform.auth_service.security;
 import com.distributed_streaming_platform.auth_service.entity.User;
 import com.distributed_streaming_platform.auth_service.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
 
-@Setter
+@Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-
     @Override
     public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
 
-
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() ->
-                        new  UsernameNotFoundException("User not found "));
+                        new UsernameNotFoundException("User not found"));
 
         return org.springframework.security.core.userdetails.User
                 .builder()
--- auth-service/src/main/java/com/distributed_streaming_platform/auth_service/security/SecurityConfig.java
+++ auth-service/src/main/java/com/distributed_streaming_platform/auth_service/security/SecurityConfig.java
@@ -2,9 +2,56 @@ package com.distributed_streaming_platform.auth_service.security;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtFilter;
+    private final JwtAuthenticationEntryPoint entryPoint;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> {})
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .exceptionHandling(ex ->
+                        ex.authenticationEntryPoint(entryPoint)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/auth/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 }
