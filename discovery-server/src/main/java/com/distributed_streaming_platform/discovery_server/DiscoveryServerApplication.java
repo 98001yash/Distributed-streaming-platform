adb61040d757b499d62fe1f373d3f022f220507b
98001yash <yashchauhan.gaya@gmail.com>
2026-03-26 12:35:23 +0530

    discovery-server added

---
+++ discovery-server/src/main/java/com/distributed_streaming_platform/discovery_server/DiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.distributed_streaming_platform.discovery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
