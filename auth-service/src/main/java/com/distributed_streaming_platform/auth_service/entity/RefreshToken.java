beed24b4f95ed1846cf0beda7b995dcc4e87b5ce
98001yash <yashchauhan.gaya@gmail.com>
2026-03-26 15:25:41 +0530

    added the refreshToken entity

---
+++ auth-service/src/main/java/com/distributed_streaming_platform/auth_service/entity/RefreshToken.java
@@ -0,0 +1,32 @@
+package com.distributed_streaming_platform.auth_service.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private LocalDateTime expiryDate;
+
+    private Boolean revoked;
+}
