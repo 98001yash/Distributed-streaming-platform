235bbb7a6b418e6b89402d3219150d1b5dd08625
98001yash <yashchauhan.gaya@gmail.com>
2026-03-27 08:19:37 +0530

    added the auth-service interface

--- auth-service/src/main/java/com/distributed_streaming_platform/auth_service/service/AuthService.java
+++ auth-service/src/main/java/com/distributed_streaming_platform/auth_service/service/AuthService.java
@@ -1,4 +1,14 @@
 package com.distributed_streaming_platform.auth_service.service;
 
+import com.distributed_streaming_platform.auth_service.dtos.AuthResponse;
+import com.distributed_streaming_platform.auth_service.dtos.LoginRequest;
+import com.distributed_streaming_platform.auth_service.dtos.RegisterRequest;
+
 public interface AuthService {
+
+    AuthResponse register(RegisterRequest request);
+
+    AuthResponse login(LoginRequest request);
+
+    AuthResponse refreshToken(String refreshToken);
 }
