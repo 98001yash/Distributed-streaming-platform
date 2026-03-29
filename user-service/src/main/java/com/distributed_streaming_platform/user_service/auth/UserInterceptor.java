0062e14f0256bda0d1a04155ab720835d6e6795c
98001yash <yashchauhan.gaya@gmail.com>
2026-03-29 07:43:24 +0530

    updated the user intercept in the user-service

--- user-service/src/main/java/com/distributed_streaming_platform/user_service/auth/UserInterceptor.java
+++ user-service/src/main/java/com/distributed_streaming_platform/user_service/auth/UserInterceptor.java
@@ -2,8 +2,10 @@ package com.distributed_streaming_platform.user_service.auth;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+@Slf4j
 public class UserInterceptor implements HandlerInterceptor {
 
 
@@ -16,13 +18,23 @@ public class UserInterceptor implements HandlerInterceptor {
         String idHeader = request.getHeader("X-User-Id");
         String role = request.getHeader("X-User-Role");
 
-        if(idHeader != null && email !=null && role !=null){
-            Long userId = Long.parseLong(idHeader);
-            UserContextHolder.setUser(userId,email, role);
+        log.info("Incoming headers → userId={}, email={}, role={}", idHeader, email, role);
+
+        try {
+            if (idHeader != null && !idHeader.isBlank() &&
+                    email != null && role != null) {
+
+                Long userId = Long.parseLong(idHeader);
+                UserContextHolder.setUser(userId, email, role);
+            } else {
+                log.warn("Missing or invalid headers for authentication");
+            }
+        } catch (Exception e) {
+            log.error("Error parsing userId from header: {}", idHeader, e);
         }
+
         return true;
     }
-
 
     @Override
     public void afterCompletion(HttpServletRequest request,
