cae0c74e69ac52015e34ebf5fc05fd0c7cc2a5e8
98001yash <yashchauhan.gaya@gmail.com>
2026-03-30 08:22:00 +0530

    added the kafka consumer in the user-service

---
+++ user-service/src/main/java/com/distributed_streaming_platform/user_service/kafka/KafkaConsumer.java
@@ -0,0 +1,24 @@
+package com.distributed_streaming_platform.user_service.kafka;
+
+
+import com.distributed_streaming_platform.events.UserCreatedEvent;
+import com.distributed_streaming_platform.user_service.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaConsumer {
+
+    private final UserService userService;
+
+    @KafkaListener(topics = "user-created", groupId = "user-service-group")
+    public void consume(UserCreatedEvent event){
+
+        log.info("Received UserCreatedEvent for userId={}",event.getUserId());
+        userService.createUser(event.getUserId(), event.getEmail());
+    }
+}
