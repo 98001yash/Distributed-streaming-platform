7e333038f41d9d9f50ec4de99f46c93f56be6b3b
98001yash <yashchauhan.gaya@gmail.com>
2026-03-30 07:54:11 +0530

    added the user created event

---
+++ common-lib/src/main/java/com/distributed_streaming_platform/events/UserCreatedEvent.java
@@ -0,0 +1,17 @@
+package com.distributed_streaming_platform.events;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCreatedEvent {
+
+    private Long userId;
+    private String email;
+}
