5df19651eec028467fee8ce4b26153d855069f6f
98001yash <yashchauhan.gaya@gmail.com>
2026-03-31 09:17:47 +0530

    added the content microservice

---
+++ content-service/src/test/java/com/distributed_streaming_platform/content_service/ContentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributed_streaming_platform.content_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
