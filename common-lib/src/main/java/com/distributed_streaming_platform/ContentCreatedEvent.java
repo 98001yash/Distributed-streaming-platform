37501eb3a85af6b0f26e907d0807d44fe329ce6b
98001yash <yashchauhan.gaya@gmail.com>
2026-03-31 14:35:33 +0530

    added the contentCrea6tedEvent

---
+++ common-lib/src/main/java/com/distributed_streaming_platform/ContentCreatedEvent.java
@@ -0,0 +1,29 @@
+package com.distributed_streaming_platform;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ContentCreatedEvent {
+
+    private String eventId;
+    private LocalDateTime eventTime;
+    private Long contentId;
+    private String title;
+    private String description;
+    private String genre;
+    private String language;
+    private String type;
+    private String status;
+
+    private String thumbnailUrl;
+    private LocalDateTime createdAt;
+}
