1f313c15163563b37b14d015775c2972572e62ad
98001yash <yashchauhan.gaya@gmail.com>
2026-03-31 15:32:51 +0530

    added the contentEventProducer

---
+++ content-service/src/main/java/com/distributed_streaming_platform/content_service/kafka/ContentEventProducer.java
@@ -0,0 +1,28 @@
+package com.distributed_streaming_platform.content_service.kafka;
+
+
+import com.distributed_streaming_platform.ContentCreatedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ContentEventProducer {
+
+
+    private final KafkaTemplate<String, ContentCreatedEvent> kafkaTemplate;
+
+    public void sendContentCreated(ContentCreatedEvent event){
+
+        log.info("Publishing contentCreatedEvent for contentId={}",event.getContentId());
+
+        kafkaTemplate.send(
+                kafkaTopics.CONTENT_CREATED,
+                String.valueOf(event.getContentId()),
+                event
+        );
+    }
+}
---
+++ content-service/src/main/java/com/distributed_streaming_platform/content_service/kafka/kafkaTopics.java
@@ -0,0 +1,6 @@
+package com.distributed_streaming_platform.content_service.kafka;
+
+public class kafkaTopics {
+
+    public static final String CONTENT_CREATED = "content_created";
+}
