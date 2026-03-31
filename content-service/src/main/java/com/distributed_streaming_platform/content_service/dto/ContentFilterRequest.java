e0c32d9ffeefc974e27d0d8758802397bafc703c
98001yash <yashchauhan.gaya@gmail.com>
2026-03-31 09:54:38 +0530

    added the content response dtos

---
+++ content-service/src/main/java/com/distributed_streaming_platform/content_service/dto/ContentFilterRequest.java
@@ -0,0 +1,20 @@
+package com.distributed_streaming_platform.content_service.dto;
+
+
+import com.distributed_streaming_platform.content_service.enums.ContentStatus;
+import com.distributed_streaming_platform.content_service.enums.ContentType;
+import com.distributed_streaming_platform.content_service.enums.Genre;
+import com.distributed_streaming_platform.content_service.enums.Language;
+import lombok.Data;
+
+@Data
+public class ContentFilterRequest {
+
+    private Genre genre;
+
+    private Language language;
+
+    private ContentType type;
+
+    private ContentStatus status;
+}
---
+++ content-service/src/main/java/com/distributed_streaming_platform/content_service/dto/ContentResponse.java
@@ -0,0 +1,34 @@
+package com.distributed_streaming_platform.content_service.dto;
+
+import com.distributed_streaming_platform.content_service.enums.ContentStatus;
+import com.distributed_streaming_platform.content_service.enums.ContentType;
+import com.distributed_streaming_platform.content_service.enums.Genre;
+import com.distributed_streaming_platform.content_service.enums.Language;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ContentResponse {
+
+    private Long id;
+
+    private String title;
+    private String description;
+
+    private ContentType type;
+    private Genre genre;
+
+    private Language language;
+    private Integer duration;
+
+    private LocalDate releaseDate;
+    private ContentStatus status;
+
+    private String thumbnailUrl;
+    private String trailerUrl;
+    private LocalDateTime createdAt;
+}
