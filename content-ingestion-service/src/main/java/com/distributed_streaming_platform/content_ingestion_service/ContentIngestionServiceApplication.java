992e819c0dc68f6cfbfe945f05911cad217ae5d9
98001yash <yashchauhan.gaya@gmail.com>
2026-04-01 09:42:23 +0530

    content-ingestion microservice added

---
+++ content-ingestion-service/src/main/java/com/distributed_streaming_platform/content_ingestion_service/ContentIngestionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.distributed_streaming_platform.content_ingestion_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentIngestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentIngestionServiceApplication.class, args);
+	}
+
+}
