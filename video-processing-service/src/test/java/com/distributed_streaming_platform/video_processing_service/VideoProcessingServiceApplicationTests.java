b3962665859539edd942046e54ddaf62dd25ab62
98001yash <yashchauhan.gaya@gmail.com>
2026-04-03 09:23:02 +0530

    video processing microservice added

---
+++ video-processing-service/src/test/java/com/distributed_streaming_platform/video_processing_service/VideoProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributed_streaming_platform.video_processing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
