935d17a1162256f37b16fcf0d3618dd1e8247dd4
98001yash <yashchauhan.gaya@gmail.com>
2026-04-03 10:30:42 +0530

    added the required enums

---
+++ video-processing-service/src/main/java/com/distributed_streaming_platform/video_processing_service/enums/ProcessingStatus.java
@@ -0,0 +1,8 @@
+package com.distributed_streaming_platform.video_processing_service.enums;
+
+public enum ProcessingStatus {
+    UPLOADED,
+    PROCESSING,
+    COMPLETED ,
+    FAILED
+}
---
+++ video-processing-service/src/main/java/com/distributed_streaming_platform/video_processing_service/enums/VideoQuality.java
@@ -0,0 +1,9 @@
+package com.distributed_streaming_platform.video_processing_service.enums;
+
+public enum VideoQuality {
+
+    P240,
+    P480,
+    P720,
+    P1080
+}
