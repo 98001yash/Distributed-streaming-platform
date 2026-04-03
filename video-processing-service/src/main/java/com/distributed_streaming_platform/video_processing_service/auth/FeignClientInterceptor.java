4eb7a797d1305c0bc219687cd15c6247b718b092
98001yash <yashchauhan.gaya@gmail.com>
2026-04-03 10:24:00 +0530

    added the auth package in the video processong service

---
+++ video-processing-service/src/main/java/com/distributed_streaming_platform/video_processing_service/auth/FeignClientInterceptor.java
@@ -0,0 +1,21 @@
+package com.distributed_streaming_platform.video_processing_service.auth;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+public class FeignClientInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate template) {
+
+        String email = UserContextHolder.getUserEmail();
+        String role = UserContextHolder.getUserRole();
+
+        if (email != null) {
+            template.header("X-User-Email", email);
+        }
+
+        if (role != null) {
+            template.header("X-User-Role", role);
+        }
+    }
+}
---
+++ video-processing-service/src/main/java/com/distributed_streaming_platform/video_processing_service/auth/RoleInterceptor.java
@@ -0,0 +1,39 @@
+package com.distributed_streaming_platform.video_processing_service.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.messaging.handler.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Arrays;
+
+public class RoleInterceptor implements HandlerInterceptor {
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler)
+            throws Exception {
+        if (!(handler instanceof HandlerMethod method)) {
+            return true;
+        }
+
+
+        RoleAllowed roleAllowed = method.getMethodAnnotation(RoleAllowed.class);
+        if (roleAllowed == null) { return true;
+        }
+
+
+        String userRole = UserContextHolder.getUserRole();
+
+        if (userRole == null || Arrays.stream(roleAllowed.value())
+                .noneMatch(role -> role.equalsIgnoreCase(userRole)))
+        {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.getWriter().write("Access Denied");
+            return false;
+        }
+        return true;
+    }
+}
