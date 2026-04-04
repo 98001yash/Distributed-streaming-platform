9e02db44aea5f0c8d60830d39d21cb3938d09a3e
98001yash <yashchauhan.gaya@gmail.com>
2026-04-04 08:42:21 +0530

    added the video-processed controller

---
+++ video-processing-service/src/main/java/com/distributed_streaming_platform/video_processing_service/controller/VideoProcessingController.java
@@ -0,0 +1,51 @@
+package com.distributed_streaming_platform.video_processing_service.controller;
+
+
+import com.distributed_streaming_platform.events.VideoUploadedEvent;
+import com.distributed_streaming_platform.video_processing_service.entity.ProcessedVideo;
+import com.distributed_streaming_platform.video_processing_service.entity.VideoVariant;
+import com.distributed_streaming_platform.video_processing_service.service.VideoProcessingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/video-processing")
+@RequiredArgsConstructor
+public class VideoProcessingController {
+
+    private final VideoProcessingService videoProcessingService;
+
+
+    @PostMapping("/process/{contentId}")
+    public String processManually(@PathVariable Long contentId) {
+
+        VideoUploadedEvent event = VideoUploadedEvent.builder()
+                .eventId(UUID.randomUUID().toString())
+                .eventTime(LocalDateTime.now())
+                .contentId(contentId)
+                .objectKey("manual/" + contentId)
+                .storageUrl("http://localhost:9000/videos/manual/" + contentId)
+                .fileName("video.mp4")
+                .build();
+
+        videoProcessingService.processVideo(event);
+
+        return "Processing started for contentId=" + contentId;
+    }
+
+
+    @GetMapping("/{contentId}")
+    public ProcessedVideo getStatus(@PathVariable Long contentId) {
+        return videoProcessingService.getByContentId(contentId);
+    }
+
+
+    @GetMapping("/{contentId}/variants")
+    public List<VideoVariant> getVariants(@PathVariable Long contentId) {
+        return videoProcessingService.getVariants(contentId);
+    }
+}
