5ff0bce1b68c2d1a5fa43c3428c8b387a69f0fc5
98001yash <yashchauhan.gaya@gmail.com>
2026-04-06 10:33:48 +0530

    added the video processing service

---
+++ streaming-service/src/main/java/com/distributed_streaming_platform/streaming_service/repository/ProcessedVideoRepository.java
@@ -0,0 +1,14 @@
+package com.distributed_streaming_platform.streaming_service.repository;
+
+
+import com.distributed_streaming_platform.streaming_service.entity.ProcessedVideo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProcessedVideoRepository extends JpaRepository<ProcessedVideo, Long> {
+
+    Optional<ProcessedVideo> findByContentId(Long contentId);
+
+    Optional<ProcessedVideo> findByContentIdAndStatus(Long contentId, String status);
+}
