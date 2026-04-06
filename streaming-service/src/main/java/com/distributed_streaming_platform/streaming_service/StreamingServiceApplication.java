43041805b2f4ded6cd1d52ad12e73ce78a9dae91
98001yash <yashchauhan.gaya@gmail.com>
2026-04-06 07:23:03 +0530

    added the streaming microservice

---
+++ streaming-service/src/main/java/com/distributed_streaming_platform/streaming_service/StreamingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.distributed_streaming_platform.streaming_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingServiceApplication.class, args);
+	}
+
+}
---
+++ streaming-service/src/test/java/com/distributed_streaming_platform/streaming_service/StreamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributed_streaming_platform.streaming_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
