e3aa3bd6ed50d3a27658c7a99712f7c7ff127a2b
98001yash <yashchauhan.gaya@gmail.com>
2026-04-08 05:32:26 +0530

    added the video progress and completed in the analytics event producer

--- streaming-service/src/main/java/com/distributed_streaming_platform/streaming_service/kafka/AnalyticsEventProducer.java
+++ streaming-service/src/main/java/com/distributed_streaming_platform/streaming_service/kafka/AnalyticsEventProducer.java
@@ -1,7 +1,8 @@
 package com.distributed_streaming_platform.streaming_service.kafka;
 
 
-import com.distributed_streaming_platform.events.VideoStartedEvent;
+
+import com.distributed_streaming_platform.events.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -14,13 +15,19 @@ public class AnalyticsEventProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    private static final String TOPIC = "video-started";
+    private static final String STARTED_TOPIC = "video-started";
+    private static final String PROGRESS_TOPIC = "video-progress";
+    private static final String COMPLETED_TOPIC = "video-completed";
 
     public void sendVideoStartedEvent(VideoStartedEvent event) {
+        kafkaTemplate.send(STARTED_TOPIC, event.getContentId().toString(), event);
+    }
 
-        log.info("Publishing VideoStartedEvent contentId={} userId={}",
-                event.getContentId(), event.getUserId());
+    public void sendVideoProgressEvent(VideoProgressEvent event) {
+        kafkaTemplate.send(PROGRESS_TOPIC, event.getContentId().toString(), event);
+    }
 
-        kafkaTemplate.send(TOPIC, event.getContentId().toString(), event);
+    public void sendVideoCompletedEvent(VideoCompletedEvent event) {
+        kafkaTemplate.send(COMPLETED_TOPIC, event.getContentId().toString(), event);
     }
 }
