a3732ef07e839bbfc40499eed1df9c3aa1cd155a
98001yash <yashchauhan.gaya@gmail.com>
2026-04-15 08:28:54 +0530

    added the configuration for the kubernates mysql

---
+++ k8s/infrastructure/mysql/init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS streaming_authdb;
+CREATE DATABASE IF NOT EXISTS streaming_userdb;
+CREATE DATABASE IF NOT EXISTS streaming_contentdb;
+CREATE DATABASE IF NOT EXISTS streaming_ingestiondb;
+CREATE DATABASE IF NOT EXISTS streaming_videoprocessingdb;
+CREATE DATABASE IF NOT EXISTS streaming_streamdb;
+CREATE DATABASE IF NOT EXISTS streaming_analyticdb;
